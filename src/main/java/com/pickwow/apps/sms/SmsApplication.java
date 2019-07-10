7a33895cd027f761fbbfbecd0e7fe20f3c236973
罗斌 <you@examople,com>
2019-07-10 23:15:02 +0800

    [罗斌]为用户控制添加了注释

--- src/main/java/com/pickwow/apps/sms/SmsApplication.java
+++ src/main/java/com/pickwow/apps/sms/SmsApplication.java
@@ -2,7 +2,9 @@ package com.pickwow.apps.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 用户控制
+ **/
 @SpringBootApplication
 public class SmsApplication {
 
